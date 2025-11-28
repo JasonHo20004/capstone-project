00fe7e82b0097da9647819a5e52ac3a64aefe436
duytm19 <tranminhduy1912@gmail.com>
2025-11-28 23:44:35 +0700

    feat(topupOrder): implement Momo payment integration and refactor order handling

--- backend/src/modules/topupOrders/routes/topupOrder.route.ts
+++ backend/src/modules/topupOrders/routes/topupOrder.route.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { TopupOrderController } from '@/modules/topupOrders/controllers/topupOrder.controller';
 import { validate } from '@/middlewares/validations.middleware';
-import { createTopupDTO, confirmPaymentDTO } from '@/modules/topupOrders/dtos/topupOrder.dto';
+import { createTopupDTO } from '@/modules/topupOrders/dtos/topupOrder.dto';
 import { authMiddleware } from '@/middlewares/auth.middleware';
 const router = Router();
 const topupOrderController = new TopupOrderController();
@@ -13,7 +13,7 @@ router.post('/create',authMiddleware,validate(createTopupDTO),topupOrderControll
 router.post(
   '/confirm-payment',
   authMiddleware,
-  validate(confirmPaymentDTO),
+ 
   topupOrderController.confirmPayment,
 );
 
